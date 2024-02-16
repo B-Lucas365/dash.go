dd8f5a7da096c42c69d070a92098f499b158861b
João Vitor <joaovitornascimentoif@gmail.com>
2024-02-16 15:50:56 -0300

    refactor - adding the login page to the home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { Input } from "@/components/Input";
+import { Container, Form } from "./styles";
+import { Button } from "@/components/Button";
+
 export default function Home() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
+    <Container>
+        <Form>
+            <Input 
+                label='E-mail' 
+                required={true} 
+                type='email'
+            />
+
+            <Input 
+                label='Senha' 
+                required={true} 
+                type='password'
+            />
+            
+            <Button text='Entrar'/>
+        </Form>
+    </Container>
+)
 } 
